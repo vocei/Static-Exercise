4390433008dfc85dff1853574f74aefbbc991ddb
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-21 13:53:04 -0400

    Finished Static  Exercise!

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FarenheitToCelsius(72);
+            Console.WriteLine($"Here is the requested temperature converted from 72 degrees Farenheit to Celsius: {celsius}");
+            Console.WriteLine("");
+            var farenheit = TempConverter.CelsiusToFarenheit(22.22222222222222);
+            Console.WriteLine($"Here is the requested temperature converted from 22.2222222 degrees Celsius to Farenheit: {farenheit}"); 
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+            return (farenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
